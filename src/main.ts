a2df6ba8718653f914be4384417892e5409dfa2a
Idan Yael <iyael@fireblocks.com>
2023-04-27 11:29:18 +0300

    added CLI

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+
 import { Command } from 'commander';
 import { ChainForkDefinition, SimulationInput } from './entities/simulation-input';
 import { Simulator } from './simulator';
